63aa252b2e04a975080d74e4b0354abe3bd15627
lerontonge <lerontonge@gmail.com>
2023-07-13 05:59:11 -0400

    hamburger icon

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client';
 import React, { useState } from 'react';
 
 const NavBar = () => {
@@ -10,15 +10,28 @@ const NavBar = () => {
 
   return (
     <nav className={isOpen ? 'nav-container open' : 'nav-container'}>
-      <div className="logo">Logo</div>
-      <div className="menu-icon" onClick={toggleNav}>
-        <i className={isOpen ? 'fas fa-times' : 'fas fa-bars'}></i>
+      <div className='logo'>Logo</div>
+      <div
+        className={`menu-icon ${isOpen ? 'open' : ''}`}
+        onClick={toggleNav}
+      >
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
-      <ul className="nav-links">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Contact</a></li>
+      <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
+        <li>
+          <a href='#'>Home</a>
+        </li>
+        <li>
+          <a href='#'>About</a>
+        </li>
+        <li>
+          <a href='#'>Services</a>
+        </li>
+        <li>
+          <a href='#'>Contact</a>
+        </li>
       </ul>
     </nav>
   );
